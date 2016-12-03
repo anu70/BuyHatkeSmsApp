5514b19089ac6f36789b9731a45f202b9061c270
anu70 <garganu70@gmail.com>
2016-12-03 20:42:10 +0530

    Integrated Search Function

--- app/src/main/java/in/org/verkstad/sms/Inbox.java
+++ app/src/main/java/in/org/verkstad/sms/Inbox.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -20,27 +21,67 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
-public class Inbox extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+public class Inbox extends AppCompatActivity /**implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener**/ {
     ListView lViewSMS;
     private GoogleApiClient mGoogleApiClient;
+    SearchView.OnQueryTextListener listener;
+    SearchView search;
+    LinkedHashSet<String> name;
+    ArrayList<ArrayList<String>> sms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
-
+        search = (SearchView) findViewById(R.id.searchView);
         lViewSMS = (ListView) findViewById(R.id.listViewSMS);
-        if(fetchInbox()!=null)
-        {
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, fetchInbox());
-            lViewSMS.setAdapter(adapter);
-        }
+        fetchInbox();
+        showInListView(new ArrayList<String>(name));
+        listener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String newText) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText.equals("")){
+                    //Toast.makeText(getApplicationContext(),"Empty",Toast.LENGTH_SHORT).show();
+                    showInListView(new ArrayList<String>(name));
+                }
+                else if(sms.size()>0){
+                    newText = newText.toLowerCase();
+                    ArrayList<String> filteredList = new ArrayList<>();
+                    Iterator<String> iterator = name.iterator();
+                    for(int i=0;i<sms.size();i++){
+                        String sender = iterator.next();
+                        for(int j=0;j<sms.get(i).size();j++){
+                            String text = sms.get(i).get(j).toLowerCase();
+                            if(text.contains(newText))
+                                filteredList.add(sender +"\n"+text);
+                        }
+                    }
+
+                    showInListView(filteredList);
+                }
+
+                return true;
+            }
+        };
+        search.setOnQueryTextListener(listener);
+
+
     }
 
-    public ArrayList fetchInbox()
+    public void showInListView(ArrayList<String> list){
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        lViewSMS.setAdapter(adapter);
+    }
+
+    public void fetchInbox()
     {
-        LinkedHashSet<String> name = new LinkedHashSet<String>();
-        final ArrayList<ArrayList<String>> sms = new ArrayList<ArrayList<String>>();
+        name = new LinkedHashSet<String>();
+        sms = new ArrayList<ArrayList<String>>();
 
         Uri uriSms = Uri.parse("content://sms/inbox");
         Cursor cursor = getContentResolver().query(uriSms, new String[]{"_id", "address", "date", "body"},null,null,null);
@@ -74,10 +115,9 @@ public class Inbox extends AppCompatActivity implements GoogleApiClient.Connecti
                 }
             });
         }
-        return new ArrayList(name);
     }
 
-    @Override
+   /** @Override
     protected void onResume() {
         super.onResume();
         if (mGoogleApiClient == null) {
@@ -105,5 +145,5 @@ public class Inbox extends AppCompatActivity implements GoogleApiClient.Connecti
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Toast.makeText(getApplicationContext(),"Connection Failed: "+connectionResult,Toast.LENGTH_SHORT).show();
-    }
+    }**/
 }
